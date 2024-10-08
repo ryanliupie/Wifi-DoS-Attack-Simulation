b6fba6a26fb816f8e383f00c353453b97ff1e3ef
ryanliupie <ryanliu314@gmail.com>
2024-10-07 20:42:50 -0400

    import done?

--- wifi_attack.py
+++ wifi_attack.py
@@ -7,8 +7,19 @@ import csv
 # this type of file (CSV) contains rows, this will help us read those rows easier. 
 import datetime
 # This will help keep track of the time and date in case we need to such as backing up a copy
+import logging
+# Tracks specific events that occur when program runs (0-7 levels)
 import os
 # Acts as a bridge between this python program and the operating system(interact with OS)
 import re
-# "regular expressions" 
+# "regular expressions" will filter out specific information from a whole set of text; separating paths
+import shutil
+# This helps move files to a different folder (CSV)
+import subprocess
+# This will help running commands on Kali Linux for us
+import tempfile
+# This will create a temporary folder for backing up existing .CSV file to reduce organizational issues
+import time
+# This is a timer to pause activites before activating them again
+
 
